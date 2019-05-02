94d48ee486235b26ff5ea8a5e4a328c5e5ac40f0
jeffau13 <jeffau13@gmail.com>
2019-05-02 18:21:41 -0400

    created routes for each post with gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          totalCount
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.slug,
+          component: path.resolve("./src/components/postLayout.js"),
+          //data passed into the component:
+          context: {
+            slug: node.frontmatter.slug,
+            //slug will be assessable within postLayout component
+          },
+        });
+      });
+      resolve();
+    });
+  });
+};
